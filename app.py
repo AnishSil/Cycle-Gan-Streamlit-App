18991bc941f7261c9c5f82b6903ff17134724e4e
Anish <anish.kumar3sil@gmail.com>
2023-05-03 21:10:41 +0530

    first

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import cv2
+import numpy as np
+from style_converter import *
+
+def app():
+    st.title("Season Translation using CycleGAN")
+    st.write("Anish Sil - 20BIT037")
+    st.write("Astha Patel - 20BIT044")
+    st.write("Parshv Patel - 20BIT065")
+
+    style="summer2winter"
+    styles = ['winter2summer', 'summer2winter']
+    style = st.selectbox('Select a style:', styles)
+    
+    season = style[:6]
+    image_file = st.file_uploader(f"Upload an {season} image file", type=["jpg", "jpeg", "png"])
+    
+    if image_file is not None:
+        image = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        st.image(image, channels='BGR', caption='Uploaded Image')
+        
+        
+        color_coverted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_1=Image.fromarray(color_coverted.astype('uint8'), 'RGB')
+
+        final=stylise(image_1,style)
+        st.image(np.array(final), caption="Processed image")
+
+if __name__ == '__main__':
+    app()
